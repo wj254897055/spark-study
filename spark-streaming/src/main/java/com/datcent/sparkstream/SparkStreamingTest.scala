50329838b9367c19d6f4d8875a7be8576e13dfb7
wujing <254897055@qq.com>
2020-01-06 10:48:03 +0800

    反压策略

--- spark-streaming/src/main/java/com/datcent/sparkstream/SparkStreamingTest.scala
+++ spark-streaming/src/main/java/com/datcent/sparkstream/SparkStreamingTest.scala
@@ -11,6 +11,10 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 object SparkStreamingTest {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setMaster("local[*]").setAppName("SparkStreamingTest")
+    //启用反压策略
+//    conf.set("spark.streaming.backpressure.enabled","true")
+    //初始反压速率 第一批数据的初始值
+//    conf.set("spark.streaming.backpressure.initialRate","10000")
     val ssc = new StreamingContext(conf,Seconds(2))
     //创建套接字流
     val lines = ssc.socketTextStream("192.168.245.210",9999)
