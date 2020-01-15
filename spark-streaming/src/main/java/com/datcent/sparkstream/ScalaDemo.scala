c967faf6881100d3c44b6187ac7fde1076043e84
wujing <254897055@qq.com>
2020-01-15 08:35:31 +0800

    ScalaDemo 测试

---
+++ spark-streaming/src/main/java/com/datcent/sparkstream/ScalaDemo.scala
@@ -0,0 +1,15 @@
+package com.datcent.sparkstream
+
+object ScalaDemo {
+
+  private def getArray[String] ={
+    ("hi","xiao","hu")
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    var strings=getArray
+    print(strings)
+  }
+
+}
