cba4e2350263317017e0f81ce909ec2fa8747c60
wujing <254897055@qq.com>
2020-03-23 14:32:14 +0800

    牛客网 在线编程

---
+++ design-modle/src/main/java/niuke/test5/Solution.java
@@ -0,0 +1,30 @@
+package niuke.test5;
+
+
+/**
+ *现在要求输入一个整数n，请你输出斐波那契数列的第n项
+ * 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233，377，610
+ */
+public class Solution {
+    public static int Fibonacci(int n) {
+        int a=0;
+        int b=1;
+        int sum=1;
+        if (n==0){
+            return 0;
+        }
+        for (int i = 1; i < n; i++) {
+            sum= a + b;
+            a=b;
+            b=sum;
+            System.out.println(sum);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+       int res= Fibonacci(2);
+        System.out.println("--------------");
+        System.out.println(res);
+    }
+}
